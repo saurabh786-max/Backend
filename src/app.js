bf4b5b91cf9e25ec6d88fc6b7ef358b1621822d6
Saurabh <saurabhshah506@gmail.com>
2025-07-31 13:59:34 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -18,4 +18,14 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
   
+// routes
+import userRouter from "./routes/user.routes.js"
+
+//Routes declaration
+app.use("/api/v1/users", userRouter) 
+
+
+
+
+
 export default app
